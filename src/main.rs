b5de8ca699cd35d89bbf9702378e0081c81fde5d
SiddharthManjul <manjulsiddharth09@gmail.com>
2024-06-05 00:04:31 +0530

    Initial Web Server with Create Task Function

--- src/main.rs
+++ src/main.rs
@@ -80,7 +80,7 @@ impl Database {
         Ok(())
     }
 
-    fn load_from_data() -> std::io::Result<Self> {
+    fn load_from_file() -> std::io::Result<Self> {
         let file_content = fs::read_to_string("database.json")?;
         let db: Database = serde_json::from_str(&file_content)?;
         Ok(db)
@@ -99,6 +99,35 @@ async fn create_task(app_state: web::Data<AppState>, task: web::Json<Task>) -> i
     HttpResponse::Ok().finish()
 }
 
-fn main() {
-    println!("Hello, world!");
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let db: Database = match Database::load_from_file() {
+        Ok(db) => db,
+        Err(_) => Database::new()
+    };
+
+    let data: web::Data<AppState> = web::Data::new(AppState {
+        db: Mutex::new(db)
+    });
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(
+                Cors::permissive()
+                    .allowed_origin_fn(|origin, _req_head| {
+                        origin.as_bytes().starts_with(b"http://localhost") || origin == "null"
+                    })
+                    .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
+                    .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+                    .allowed_header(header::CONTENT_TYPE)
+                    .supports_credentials()
+                    .max_age(3600)
+            )
+            .app_data(data.clone())
+            .route("/task", web::post().to(create_task))
+
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
